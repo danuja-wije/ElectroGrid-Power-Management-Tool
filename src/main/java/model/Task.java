166c702b52bd3a85fa04b60b5d6c726dcf4c020d
danuja-wije <danujawijerathne45@gmail.com>
2022-04-12 14:03:48 +0530

    add task services to view,insert crud

--- src/main/java/model/Task.java
+++ src/main/java/model/Task.java
@@ -1,5 +1,126 @@
 package model;
 
-public class Task {
+import java.sql.*;
 
+public class Task {
+	private static final String USERNAME = "root";
+	private static final String URL = "jdbc:mysql://127.0.0.1:3306/electrogriddb";
+	private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+	private static final String PASSWORD = "";
+	private static Connection connection = null;
+	private static String query = "";
+	private static PreparedStatement preparedStatement = null;
+	private static Statement statement = null;
+	private static ResultSet resultSet = null;
+	
+	
+	private Connection connect() throws SQLException {
+		if (connection != null && !connection.isClosed()) {
+			return connection;
+		}
+		else {
+			try {
+				Class.forName(DRIVER);
+				connection = DriverManager.getConnection(URL,USERNAME,PASSWORD);
+				System.out.println("Successfully Connected");
+			} catch (Exception e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+			return connection;
+		}
+	}
+	public String viewTask(int taskID) {
+		String output = "";
+		
+		try {
+			connection = connect();
+			
+			if(connection == null) {
+				output = "Error while connecting to the database for inserting";
+				return output;
+			}
+			
+			query = "select * from task where taskID = "+taskID;
+			String query_workers = "select * from task_workers where taskID = "+taskID;
+			statement = connection.createStatement();
+	
+			resultSet = statement.executeQuery(query);
+			
+
+			while(resultSet.next()) {
+				String title = resultSet.getString("title");
+				String desc = resultSet.getString("description");
+				String handleBy = resultSet.getString("handleBy");
+				String status = resultSet.getString("status");
+				String created =resultSet.getString("createTime");
+				String lastUpdate = resultSet.getString("lastUpdate");
+				output =  "<label>Task Title :"+title+"</label>" + "</br>"
+						+ "<label>Task Description :"+desc+"</label>" + "</br>"
+						+ "<label>Handle BY :"+handleBy+"</label>" + "</br>"
+						+"<label>Status :"+status+"</label>" + "</br>"
+						+ "<label>Created Date :"+created+"</label>" + "</br>"
+						+"<label>Last Update :"+lastUpdate+"</label>" + "</br>";
+			}
+			
+			ResultSet worker_rs = statement.executeQuery(query_workers);
+			
+			output  +=  "<table border='1'><tr><th>Worker ID</th><th>Action</th>" +
+					"</tr>";
+
+			while(worker_rs.next()) {
+				String workerID = worker_rs.getString("workerID");
+				output +=  "<tr><td>"+workerID+"</td>"
+								 + "<td><form method='post' action='items.jsp'>"
+								 + "<input name='btnUpdate' type='button' value='Update' class='btn btn-secondary'>"
+								 + "<input name='btnRemove' type='submit' value='Remove' class='btn btn-danger'>"
+								 + "<input name='itemID' type='hidden' value='" + workerID 
+								 + "</form></td></tr>"; 
+			}
+			
+			output += "</table>";
+			
+			
+			connection.close();
+			query = "";
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			output = "Error while reading tasks";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
+	
+
+	public String insertWorkers(String taskID,String workerID) {
+		String output = "";
+		try {
+			connection  = connect();
+			if (connection == null ) {
+				output = "Error while connectiong to the database for Inserting";
+				return output;
+			}
+
+			query = "INSERT INTO `task_workers` (`taskID`, `workerID`) VALUES (?, ?)";
+			
+			preparedStatement = connection.prepareStatement(query);
+			
+			preparedStatement.setString(1, taskID);
+			preparedStatement.setString(2, workerID);
+			
+			preparedStatement.execute();
+			
+			connection.close();
+			
+			output = "Inserted Successfully";
+			query = "";
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			output = "Error while inserting the workers";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
 }
--- src/main/java/service/TaskService.java
+++ src/main/java/service/TaskService.java
@@ -1,5 +1,6 @@
 package service;
-
+//load Models
+import model.Task;
 //For REST Service
 import javax.ws.rs.*; 
 import javax.ws.rs.core.MediaType; 
@@ -12,10 +13,30 @@ import org.jsoup.nodes.Document;
 
 @Path("/Task")
 public class TaskService {
+	
+	Task task = new Task();
+	
 	@GET
-	@Path("/")
-	@Produces(MediaType.TEXT_PLAIN)
-	public String TestTask() {
-		return "Test Complete";
+	@Path("/{taskID}")
+	@Produces(MediaType.TEXT_HTML)
+	public String viewTask(@PathParam("taskID") int taskID) {
+		return task.viewTask(taskID);
 	}
+	
+	
+	
+	
+	
+	@POST
+	@Path("/Worker")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String insertWorker(@FormParam("taskID") String taskID , @FormParam("workerID") String workerID) {
+		System.out.println("Task ID :"+taskID);
+		System.out.println("Worker ID :"+workerID);
+		String output = task.insertWorkers(taskID, workerID);
+		return output;
+	}
+	
+	
 }
