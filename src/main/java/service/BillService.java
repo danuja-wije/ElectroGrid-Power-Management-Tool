7c4e24cc2ed60bec37fd650371f5f390828e7e70
salitha10 <salithaniranjana@gmail.com>
2022-04-19 12:59:27 +0530

    Bill Service

---
+++ src/main/java/service/BillService.java
@@ -0,0 +1,15 @@
+package service;
+
+import java.util.ArrayList;
+
+import model.Bill;
+
+public interface BillService {
+	
+	public String generateBill(Bill bill);
+	public String deleteBill(long cardNumber);
+	public ArrayList<Bill> viewAllBills(String user_ID);
+	public ArrayList<Bill> viewMonthlyBills(String user_ID, String Month);
+	public String updateBill(String bill_ID, Bill bill);
+	
+}
