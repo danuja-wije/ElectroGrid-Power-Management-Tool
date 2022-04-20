7e4e9f26b4cfa15ed9329abc28b095637cc24013
salitha10 <salithaniranjana@gmail.com>
2022-04-20 16:26:30 +0530

    bill add, delete

--- src/main/java/service/BillService.java
+++ src/main/java/service/BillService.java
@@ -7,7 +7,7 @@ import model.Bill;
 public interface BillService {
 	
 	public String generateBill(Bill bill);
-	public String deleteBill(long cardNumber);
+	public String deleteBill(long bill_ID);
 	public ArrayList<Bill> viewAllBills(String user_ID);
 	public ArrayList<Bill> viewMonthlyBills(String user_ID, String Month);
 	public String updateBill(String bill_ID, Bill bill);
