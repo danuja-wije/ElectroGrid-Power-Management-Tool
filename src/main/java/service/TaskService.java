e1f8fe445c53a206ade3564f4c8b85c9c00e9863
salitha10 <salithaniranjana@gmail.com>
2022-04-21 11:23:54 +0530

    before final test

--- src/main/java/service/TaskService.java
+++
@@ -1,9 +0,0 @@
-package service;
-
-import model.Task;
-
-public interface TaskService {
-	public Task viewTask(int taskID);
-	public String insertTask(Task task);
-	public String insertWorkers(String taskID,String workerID);
-}
