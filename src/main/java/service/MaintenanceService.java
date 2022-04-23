2c36fa4d16d30170fc5d29f2e7b25cd5dfb6ff2b
Danuja Wijerathna <61626598+danuja-wije@users.noreply.github.com>
2022-04-23 21:20:17 +0530

    add crud retrive/add/update/delete in interruption

--- src/main/java/service/MaintenanceService.java
+++ src/main/java/service/MaintenanceService.java
@@ -1,5 +1,13 @@
 package service;
 
-public interface MaintenanceService {
+import java.util.List;
 
+import model.Interruption;
+
+public interface MaintenanceService {
+	public String insertInterruption(Interruption interruption);
+	public List<Interruption> allInterruptions();
+	public String updateInterruption(Interruption interruption);
+	public String updateEffectedCustomer(List<String>list);
+	public String deleteInterruption(int id);
 }
