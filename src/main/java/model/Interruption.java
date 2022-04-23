47181165b43090a934871c6678893aac0b028547
Danuja Wijerathna <61626598+danuja-wije@users.noreply.github.com>
2022-04-23 21:20:17 +0530

    add crud retrive/add/update/delete in interruption

---
+++ src/main/java/model/Interruption.java
@@ -0,0 +1,69 @@
+package model;
+
+import java.util.List;
+
+public class Interruption {
+	private int id;
+	private String inType;
+	private String title;
+	private String description;
+	private String interruptionStartDate;
+	private String interruptionEndDate;
+	private List<Employee>efectedList;
+	private String approval;
+	private String timeStamp;
+	
+	public Interruption(String inType, String title, String description, String interruptionStartDate,
+			String interruptionEndDate, List<Employee> efectedList, String approval) {
+		super();
+		this.inType = inType;
+		this.title = title;
+		this.description = description;
+		this.interruptionStartDate = interruptionStartDate;
+		this.interruptionEndDate = interruptionEndDate;
+
+		this.efectedList = efectedList;
+		this.approval = approval;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getInType() {
+		return inType;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public String getInterruptionStartDate() {
+		return interruptionStartDate;
+	}
+
+	public String getInterruptionEndDate() {
+		return interruptionEndDate;
+	}
+
+	public List<Employee> getEfectedList() {
+		return efectedList;
+	}
+
+	public String getApproval() {
+		return approval;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setTimeStamp(String timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+	
+}
