98da81333e0000a9aad686f04c3a17752f3e7042
heshari-deSaram <hesharidesaram@gmail.com>
2022-04-24 14:41:33 +0530

    View Consumer

---
+++ src/main/java/controller/ProfileController.java
@@ -0,0 +1,18 @@
+package controller;
+
+import model.Consumer;
+
+//For REST Service
+import javax.ws.rs.*; 
+import javax.ws.rs.core.MediaType; 
+//For JSON
+import com.google.gson.*; 
+//For XML
+import org.jsoup.*; 
+import org.jsoup.parser.*; 
+import org.jsoup.nodes.Document; 
+
+@Path("/Profile")
+public class ProfileController {
+
+}
