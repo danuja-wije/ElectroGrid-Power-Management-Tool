9c40f01434c69f74ce47a74ae2201a4515a2c3b5
Danuja Wijerathna <61626598+danuja-wije@users.noreply.github.com>
2022-04-25 03:44:54 +0530

    implement basuc Auth

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -1,19 +1,27 @@
 package controller;
 
 import javax.ws.rs.GET;
-
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import service.LoginService;
+import service.LoginServiceImpl;
+
 
 @Path("/Login")
 public class LoginController {
+	LoginService loginService = new LoginServiceImpl();
 	@GET
 	@Path("/Validate")
 	@Produces(MediaType.TEXT_PLAIN)
-
-	public String checkLogin() {
-		return "success";
+	public String checkLogin(@HeaderParam("Authorization") String auth) {
+		
+		if(loginService.isAuthenticated(auth)) {
+			return "Success";
+		}else {
+			return "failed";
+		}
 	}
 }
