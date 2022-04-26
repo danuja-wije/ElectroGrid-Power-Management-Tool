8e9acff3c11c082bba57e3cb9bb3c6e361ddd0a3
Rimzan Deen <56365282+tuanrimzan@users.noreply.github.com>
2022-04-26 21:23:11 +0530

    Updated AdminManageService.java

--- src/main/java/service/AdminManageService.java
+++ src/main/java/service/AdminManageService.java
@@ -5,7 +5,7 @@ import model.Employee;
 
 public interface AdminManageService {
 	public String insertEmployee(Employee employee);
-	public ArrayList<Employee> viewEmployees(String empID);
+	public ArrayList<Employee> viewEmployees();
 	public String updateEmployee(String empID, Employee employee);
 	public String deleteEmployee(String empID);
 }
